f81a9f5703e949bcca5e35293f3ce052f04eca5d
headwing <110597846+headwing@users.noreply.github.com>
2022-12-01 10:56:35 +0900

    주특기 입문 주차 시험!!!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useRef } from "react";
 
 function App() {
+  let [todo, setTodo] = useState(["리액트를 공부해봅시다.", "푹 쉬기"]);
+  let [tempTodo, setTempTodo] = useState("");
+
+  const addTodoHandler = (e) => {
+    let copy = [...todo];
+    copy.push(tempTodo);
+    setTodo(copy);
+  };
+
+  const inputRef = useRef(null);
+
+  const clearInput = () => {
+    inputRef.current.value = "";
+    setTempTodo("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        onChange={(e) => {
+          setTempTodo(e.target.value);
+        }}
+        ref={(element) => (inputRef.current = element)}
+      />
+      <button
+        onClick={(e) => {
+          addTodoHandler();
+          clearInput();
+        }}
+      >
+        추가하기
+      </button>
+
+      <h1>Todo List</h1>
+
+      <div className="todoList">
+        {todo.map((a, i) => {
+          return <div className="todoListIndi">{a}</div>;
+        })}
+      </div>
     </div>
   );
 }
